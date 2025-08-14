d72a94866c8211fcbcfbb3d0f80571386d3babf4
Sam Pasmann <spasmann@nd.edu>
2025-08-14 17:13:59 -0400

    Removed manifold dagmc.h5m from git history

--- examples/model_manifold/input.py
+++ examples/model_manifold/input.py
@@ -3,6 +3,29 @@ import openmc
 # Example adapted from the steel pip manifold example here:
 # https://docs.openmc.org/en/v0.12.1/examples/unstructured-mesh-part-ii.html
 
+from IPython.display import display, clear_output
+import urllib.request
+
+manifold_geom_url = 'https://tinyurl.com/rp7grox' # 99 MB
+manifold_mesh_url = 'https://tinyurl.com/wojemuh' # 5.4 MB
+
+def download(url, filename):
+    """
+    Helper function for retrieving dagmc models
+    """
+    def progress_hook(count, block_size, total_size):
+        prog_percent = 100 * count * block_size / total_size
+        prog_percent = min(100., prog_percent)
+        clear_output(wait=True)
+        display('Downloading {}: {:.1f}%'.format(filename, prog_percent))
+
+    urllib.request.urlretrieve(url, filename, progress_hook)
+
+# get the manifold DAGMC geometry file
+download(manifold_geom_url, 'dagmc.h5m')
+# get the manifold tet mesh
+download(manifold_mesh_url, 'unstructured_mesh.h5m')
+
 # Materials
 air = openmc.Material(name='air')
 air.set_density('g/cc', 0.001205)
